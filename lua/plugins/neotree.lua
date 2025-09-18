614e38df7b7c155a4ab07b91a957fd7aa8856216
Nikolai Vasilenko <nikolai-vasilenko-developer@yandex.ru>
2025-09-18 14:03:44 +0700

    Set hard size to neo tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,5 +8,14 @@ return {
           "nvim-tree/nvim-web-devicons", -- optional, but recommended
         },
         lazy = false, -- neo-tree will lazily load itself
+        config = function () 
+            local conf = require('neo-tree')
+            conf.setup({
+                window = {
+                    max_width = 35,
+                    width = .19,
+                },
+            })
+        end
     },
 }
