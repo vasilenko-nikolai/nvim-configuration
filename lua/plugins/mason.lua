a49f5eccbac67090f12e41dfd51707a43e280070
Nikolai Vasilenko <nikolai-vasilenko-developer@yandex.ru>
2025-09-19 16:44:08 +0700

    Setting up Mason for portable assembly

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,5 +2,19 @@ return {
     {
         'williamboman/mason.nvim',
         opts = {}
-    }
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true,
+        config = function ()
+            local mason_lspconfig = require('mason-lspconfig')
+            mason_lspconfig.setup({
+                ensure_installed = require('utils.lsp_servers'),
+                automatic_installation = true,
+            }) 
+        end,
+        opts = {
+            auto_install = true, -- Главная настройка для автоматизации
+        },
+    },
 }
---
+++ lua/utils/lsp_servers.lua
@@ -0,0 +1,14 @@
+return {
+    "lua_ls",
+    "emmet-language-server",
+    "eslint-lsp",
+    "eslint_d",
+    "gopls",
+    "postgrestools",
+    "pyright",
+    "ruff",
+    "rust-analyzer",
+    "typescript-language-server",
+    "vtsls",
+    "vue-language-server",
+}
